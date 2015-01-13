8e261d9f21201b95ffa2c8f28f3a290802c524af
沈霖 <saleemshenlin@gmail.com>
2015-01-13 16:26:57 +0800

    finish weibo schema

--- NodeJSDemo/NodejsDemoApp1/NodejsDemoApp1/model/poi_schema.js
+++ NodeJSDemo/NodejsDemoApp1/NodejsDemoApp1/model/poi_schema.js
@@ -15,3 +15,40 @@ poiSchema.methods.print = function () {
         : "This has not title!";
     console.log(printName);
 }
+
+/*
+
+insert poi from mergeresult_final
+
+
+
+var Poi = mongoose.model('POI', poiSchema); 
+fs.readFile('./datas/mergeresult_final.json', function (err, data) {
+    if (err)
+        throw err;
+
+    var jsonObj = JSON.parse(data);
+    var space = ' ';
+    var newLine = ' . ';
+    var chunks = [];
+    var length = 0;
+
+    for (var i = 0; i < jsonObj.length ; i++) {
+        var item = jsonObj[i];
+        var poiItem = new Poi({
+            poiid: item['poiid'],
+            title: item['title'],
+            lon: item['lon'],
+            lat: item['lat'],
+            category: item['category'],
+            category_name: item['category_name'],
+            address: item['address']
+        })
+        poiItem.save(function (err, poiItem) {
+            if (err) return console.error(err);
+            poiItem.print();
+        });
+    }
+});
+
+*/
